d0f5f0275d6a6a45a2fd7bb36a9dd3f0d81d340e
Mario <marizqlav@alum.us.es>
2022-11-03 14:47:09 +0100

    Simulador COntrol Check

--- src/main/java/org/springframework/samples/petclinic/product/ProductController.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductController.java
@@ -1,5 +1,57 @@
 package org.springframework.samples.petclinic.product;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.owner.Owner;
+import org.springframework.samples.petclinic.pet.Pet;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class ProductController {
-    
+	
+	private ProductService productService;
+	
+	@Autowired
+	public ProductController(ProductService productService) {
+		this.productService=productService;
+	}
+	
+	@InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+	
+	@ModelAttribute("types")
+    public List<ProductType> populateProductTypes() {
+        return this.productService.findAllProductTypes();
+    }
+
+	@GetMapping(value = "/product/create")
+	public String initCreationForm(Map<String, Object> model) {
+		Product product = new Product();
+		model.put("product", product);
+		return "products/createOrUpdateProductForm";
+	}	
+	
+	@PostMapping(value = "/product/create")
+	public String processCreationForm(@Valid Product product, BindingResult result, ModelMap model) {
+		if (result.hasErrors()) {
+			return "products/createOrUpdateProductForm";
+		}
+		else {
+			this.productService.save(product);
+			return "welcome";
+		}
+	}   
 }
--- src/main/java/org/springframework/samples/petclinic/product/ProductTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductTypeFormatter.java
@@ -1,24 +1,37 @@
 package org.springframework.samples.petclinic.product;
 
 import java.text.ParseException;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductTypeFormatter implements Formatter<ProductType>{
 
+	private ProductService productService;
+	
+	@Autowired
+	public ProductTypeFormatter(ProductService productService) {
+		this.productService=productService;
+	}
+	
     @Override
     public String print(ProductType object, Locale locale) {
-        // TODO Auto-generated method stub
-        return null;
+        return object.getName();
     }
 
     @Override
     public ProductType parse(String text, Locale locale) throws ParseException {
-        // TODO Auto-generated method stub
-        return null;
+    	List<ProductType> productTypes=productService.findAllProductTypes();
+    	for (ProductType p:productTypes) {
+			if (p.getName().equals(text)) {
+				return p;
+			}
+		} throw new ParseException("ProductType not found"+ text, 0);
     }
     
 }
